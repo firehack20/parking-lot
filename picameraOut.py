55899b3895ade756078b5215e536498644bce137
nguyen <73287218+firehack20@users.noreply.github.com>
2021-12-15 09:24:09 +0700

    Update picameraOut.py

--- picameraOut.py
+++ picameraOut.py
@@ -21,22 +21,26 @@ content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 licenseNumber='a'
 tokens=[]
+tryCount=0
 
 def saveOut(doc_ref):
 	tokens.clear()
+	print('Xe dang trong bai, cho xe di ra')
 	get_id= doc_ref.get({u'id'})
 	id= u'{}'.format(get_id.to_dict()['id'])
+	print('id:',id)
 	users_ref=db.collection(u'Users').document(id)
 	#admin=admin=='True'
 	get_token=users_ref.get({u'token'})
 	token=u'{}'.format(get_token.to_dict()['token'])
+	users_ref.update({u'haveNotification':True})
 	checkin=False #ra
 	tokens.append(token)
 	sendPush("xac nhan lay xe", "xac nhan lay xe?", tokens)
+	print('Dang doi xac nhan lay xe...')
 	res = requests.get('http://detectparking.ddns.net/auth/').text 
 	while res == 'None':
 		res = requests.get('http://detectparking.ddns.net/auth/').text
-		print('wait')
 		continue
 	dict=res
 	res = json.loads(dict)
@@ -58,6 +62,7 @@ def saveOut(doc_ref):
 		}
 		db.collection(u'TimeInOut').add(data)
 		print('saved!')
+		servo.dongmocong()
 	else:
 		print('an trom')
 
@@ -67,28 +72,30 @@ while True:
 		output = cv2.cvtColor(output , cv2.COLOR_BGR2RGB)
 		_, img_encoded= cv2.imencode('.jpg',output)
 		response=requests.post('http://detectparking.ddns.net/predict/',headers=headers ,data=img_encoded.tobytes())
-		print(response.text)
 		if len(response.text) > 1:
+			print(response.text)
 			typeVehicle, license1 = response.text.split('-')
 			if not checkIn.TrueResult(typeVehicle,license1):
-				print('ko du ky tu')
+				print('khong du ky tu')
+				tryCount=0
 				continue
-			#license1=input()
-			print('licenseNumber',licenseNumber)
-			if licenseNumber!=license1:
-				print('here')
+			elif licenseNumber!=license1:
+				print(license1)
 				doc_ref=db.collection(u'LicensePlateNumber').document(license1)
 				doc=doc_ref.get()
+				tryCount+=1
+				#print(tryCount)
 				if doc.exists:
 					if checkIn.ParkingCheck(doc_ref,db): #False
-						print('xe trong bai')
 						saveOut(doc_ref)
-						servo.dongmocong()
+						tryCount=0
 					else:
-						print('o ngoai roi')
+						print('Xe dang o ngoai roi')
+						tryCount=0
 						
-				else: 
-					sendToAdmin(img_encoded,'Kiểm tra xe ra')
+				elif tryCount>2: 
+					print('Loi nhan dien')
+					sendToAdmin(img_encoded,'Kiem tra xe ra')
 					license1 = requests.get('http://detectparking.ddns.net/selfPredict/').text
 					while license1=='None':
 						license1 = requests.get('http://detectparking.ddns.net/selfPredict/').text
@@ -103,16 +110,20 @@ while True:
 					doc=doc_ref.get()
 					if doc.exists:#true
 						if checkIn.ParkingCheck(doc_ref,db):
-							
-							print('xe trong bai')
 							saveOut(doc_ref)
-							servo.dongmocong()					
+							tryCount=0
 						else:
-							print('luu roi di ra khoi lan')
+							print('Luu roi di ra khoi lan')
+							tryCount=0
 					else:
 						print('Tai khoan da bi xoa')
-			licenseNumber= license1
-			print('Trung lap bien so')
+						tryCount=0
+				licenseNumber= license1
+			else:
+				print('Trung lap bien so')
+				tryCount=0
 		else:
 			print('khong co xe')
+			tryCount=0
 			continue
+
